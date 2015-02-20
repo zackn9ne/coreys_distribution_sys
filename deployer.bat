2765e48fefc20ea961f10a9e30fde917df1a37bb
zackn9ne <zackn9ne@gmail.com>
2015-02-19 19:51:01 -0500

    another deployment

---
+++ deployer.bat
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -R . ~/code/universal-heroku-deployer
+echo "copied to universal-heroku-deployer"
+cd ~/code/universal-heroku-deployer
+echo "changed dir to universal heroku deployer"
+open index.html
